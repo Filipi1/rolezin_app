b417daeb2a3210ce79d644ac3264a5bd046293ab
Filipe <filipebragabrasil@hotmail.com>
2025-01-26 23:05:26 -0300

    feat: moved room entity mock to a reutilizable function

--- test/infra/repositories/room_repository_test.dart
+++ test/infra/repositories/room_repository_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rolezin_app/domain/entities/room.entity.dart';
@@ -10,31 +11,39 @@ class MockHttpAdapter extends Mock implements HttpAdapter {}
 class MockHttpBaseOptions extends Mock implements HttpBaseOptions {}
 
 void main() {
+  Faker faker = Faker();
+
   setUpAll(() {
     registerFallbackValue(MockHttpBaseOptions());
   });
 
+  RoomEntity mockRoomEntity() {
+    RoomEntity mockEntity = MockRoomEntity();
+    double bruteBalance = faker.randomGenerator.decimal();
+    when(() => mockEntity.toJson()).thenReturn({
+      "id": faker.randomGenerator.integer(9999),
+      "name": faker.randomGenerator.string(50),
+      "geolocation": null,
+      "partners": <Map<String, dynamic>>[],
+      "items": <Map<String, dynamic>>[],
+      "establishmentBonusPercent": faker.randomGenerator.integer(10),
+      "bruteBalance": bruteBalance,
+      "totalBalance": faker.randomGenerator.decimal(min: bruteBalance),
+      "memories": <Map<String, dynamic>>[],
+      "createdAt": "2023-05-20T00:00:00"
+    });
+    return mockEntity;
+  }
+
   test("should request with correct value when call fetchRoom", () async {
     HttpAdapter httpAdapter = MockHttpAdapter();
-    RoomEntity mockEntity = MockRoomEntity();
-    when(() => mockEntity.toJson()).thenReturn({
-        "id": 1,
-        "name": "Role na casa de praia!",
-        "geolocation": null,
-        "partners": <Map<String, dynamic>>[],
-        "items": <Map<String, dynamic>>[],
-        "establishmentBonusPercent": 10,
-        "bruteBalance": 100.55,
-        "totalBalance": 110.90,
-        "memories": <Map<String, dynamic>>[],
-        "createdAt": "2023-05-20T00:00:00"
-    });
-    HttpResponse result = HttpResponse(status: 200, body: mockEntity.toJson());
+    RoomEntity roomEntity = mockRoomEntity();
+    HttpResponse result = HttpResponse(status: 200, body: roomEntity.toJson());
     when(() => httpAdapter.request(options: any(named: "options"))).thenAnswer((options) async => Future.value(result));
     final repository = RoomRepository(adapter: httpAdapter);
 
     RoomEntity? sut = await repository.fetchRoom(roomId: 10);
 
-    expect(sut, RoomEntity.fromJson(mockEntity.toJson()));
+    expect(sut, RoomEntity.fromJson(roomEntity.toJson()));
   });
 }
